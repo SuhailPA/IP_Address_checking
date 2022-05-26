b7731bd4089a6f290d2c92af665c3217a8fbfbe7
Suhail <Suhail@vajro.com>
2022-05-26 18:37:04 +0530

    Coding completed

--- app/src/main/java/com/example/ipcheckingapp/MainActivity.kt
+++ app/src/main/java/com/example/ipcheckingapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.ipcheckingapp
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.Formatter
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "KotlinApp"
+        val textView: TextView = findViewById(R.id.getIPAddress)
+        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val ipAddress: String = Formatter.formatIpAddress(wifiManager.connectionInfo.ipAddress)
+        textView.text = "Your Device IP Address: $ipAddress"
     }
 }
