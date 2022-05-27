1b932cabeb04c1c388d70519daad85e4a3d8e033
Suhail <Suhail@vajro.com>
2022-05-27 16:40:55 +0530

    *Added the code for getting the IP for both mobile network and wifi

--- app/src/main/java/com/example/ipcheckingapp/MainActivity.kt
+++ app/src/main/java/com/example/ipcheckingapp/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.ipcheckingapp
 
 import android.content.Context
-import android.net.wifi.WifiManager
-import androidx.appcompat.app.AppCompatActivity
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
 import android.os.Bundle
-import android.text.format.Formatter
 import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var network : String
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        network = getNetwork()
         setContentView(R.layout.activity_main)
         title = "KotlinApp"
         val textView: TextView = findViewById(R.id.getIPAddress)
-        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        val ipAddress: String = Formatter.formatIpAddress(wifiManager.connectionInfo.ipAddress)
-        textView.text = "Your Device IP Address: $ipAddress"
+        val viewModel : MainViewModel by viewModels()
+        textView.text = "Your Device IP Address: $network : ${viewModel.getIpAddress() ?: "Not connected to network"}"
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onResume() {
+        super.onResume()
+        network = getNetwork()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun getNetwork(): String {
+        val connectivityManager =
+            applicationContext.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val nw = connectivityManager.activeNetwork ?: return "-"
+        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return "-"
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> "WIFI"
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> "ETHERNET"
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> "MOBILE NETWORK"
+            else -> "Something went wrong"
+        }
     }
 }
---
+++ app/src/main/java/com/example/ipcheckingapp/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.ipcheckingapp
+
+import androidx.lifecycle.ViewModel
+import java.net.Inet4Address
+import java.net.NetworkInterface
+import java.net.SocketException
+
+class MainViewModel : ViewModel() {
+
+    fun getIpAddress(): String? {
+        try {
+            val en = NetworkInterface.getNetworkInterfaces()
+            while (en.hasMoreElements()) {
+                val intf = en.nextElement()
+                val enumIpAddr = intf.inetAddresses
+                while (enumIpAddr.hasMoreElements()) {
+                    val inetAddress = enumIpAddr.nextElement()
+                    if (!inetAddress.isLoopbackAddress && inetAddress is Inet4Address) {
+                        return inetAddress.hostAddress
+                    }
+                }
+            }
+        } catch (ex: SocketException) {
+            ex.printStackTrace()
+        }
+        return null
+    }
+}
